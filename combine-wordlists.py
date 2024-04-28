eb0f2d81a21c28b8fd11daf53427d32a8785749e
keeganvolk <keeganavolk@gmail.com>
2024-04-28 12:45:16 -0500

    commit

---
+++ combine-wordlists.py
@@ -0,0 +1,13 @@
+import sys
+import argparse
+p=argparse.ArgumentParser(description="",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+p.add_argument("[output file]",help="File name to output to")
+p.add_argument("[input files]",nargs=argparse.REMAINDER,help="Names of wordlist files to combine")
+args=p.parse_args()
+a=vars(args)
+outstring=''
+for i in a["[input files]"]:
+ with open(i) as file:
+  outstring+=file.read()+'\n'
+with open(a["[output file]"],'w') as file:
+ file.write(outstring)
